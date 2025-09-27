3f00bac400c14b0af9e6fd0db9b965a4c759ba2d
314159265789 <altendy39@gmail.com>
2025-09-27 08:23:16 +0300

    Initial site upload

---
+++ Site/script.js
@@ -0,0 +1,8 @@
+// Example interactive feature
+document.addEventListener("DOMContentLoaded", () => {
+  const contactBtn = document.getElementById("contactBtn");
+
+  contactBtn.addEventListener("click", () => {
+    alert("Thank you for reaching out! Our support team will contact you soon.");
+  });
+});
